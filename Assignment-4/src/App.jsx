e58faf4880e8162e9e1fb508fbe8a61c8e8aabc2
CarsonChan07 <carson.chan@student.tdsb.on.ca>
2025-04-01 13:24:30 -0400

    Edited Code

--- Assignment-4/src/App.jsx
+++ Assignment-4/src/App.jsx
@@ -2,6 +2,7 @@ import HeronsFormula from "./components/HeronsFormula.jsx";
 import NewtonsMethod from "./components/NewtonsMethod.jsx";
 import AmbiguousCase from "./components/AmbiguousCase.jsx";
 import PolynomialFunction from "./components/PolynomialFunction.jsx";
+import "./components/style.css"
 
 function App() {
   return (
